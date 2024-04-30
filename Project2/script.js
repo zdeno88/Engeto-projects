20948e2811f5084fe58110ef8eda760c04e35438
Zdenek Skaryd <zdenek.skaryd@csobpoj.cz>
2024-04-30 13:15:28 +0200

    projekt2 complete

---
+++ Project2/script.js
@@ -0,0 +1,22 @@
+const icon = document.querySelector(".fa-solid");
+const nav = document.querySelector("nav");
+const header = document.querySelector("#home");
+console.log(header);
+let cond = true;
+icon.addEventListener("click", () => {
+  if (cond) {
+    nav.style.display = "block";
+    icon.classList.remove("fa-bars");
+    icon.classList.add("fa-xmark");
+    cond = false;
+  } else {
+    nav.style.display = "none";
+    icon.classList.add("fa-bars");
+    icon.classList.remove("fa-xmark");
+    cond = true;
+  }
+});
+window.addEventListener("scroll", () => {
+  console.log("scroluji");
+  header.style.position = "fixed;";
+});
